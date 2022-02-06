bed5e90a97ca9302f3658cf577a7a780c42c25a3
Jasoncarterjoseph <jaesonj52@gmail.com>
2022-02-07 02:02:17 +0530

    fl

--- app.js
+++ app.js
@@ -11,10 +11,17 @@ require('./config/database.config')(CONFIG[env]);
 require('./config/express')(APP);
 require('./config/routes')(APP);
 
-APP.use(EXPRESS.static(path.join(__dirname, 'public')));
-APP.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
-  });
+if (process.env.NODE_ENV === 'production') {
+	APP.use(EXPRESS.static('public'));
+}
+
+APP.get('*', (request, response) => {
+	response.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+// APP.use(EXPRESS.static(path.join(__dirname, 'public')));
+// APP.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public/index.html'));
+//   });
 
 APP.listen(PORT);
 console.log(`Server is listening on port ${PORT}`);
