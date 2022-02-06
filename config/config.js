9b7fa8dc99d9e04fb6ac045bc82130d2b800215e
Jasoncarterjoseph <jaesonj52@gmail.com>
2022-02-07 01:09:38 +0530

    Added Public new pass

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     development: {
-        connectionString: 'mongodb+srv://jaesonj52:Julian@2002@cluster0.brbrb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+        connectionString: 'mongodb+srv://jaesonj52:Julian%402002@cluster0.brbrb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
     },
     production: {
 
